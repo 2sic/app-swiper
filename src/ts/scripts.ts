d6bb8624a86de7baedd97d1b2dade66936579729
Roman Opalko <roman.opalko@2sic.com>
2020-05-04 16:25:30 +0200

    moved js out of template

---
+++ src/ts/scripts.ts
@@ -0,0 +1,10 @@
+import Swiper from 'swiper';
+
+(window as any).appSwiperInit = function appSwiperInit(moduleID: any, autoplay: string, speed: string, effectDefaults: any, fallback: any) {
+  var configured = {
+    autoplay: autoplay,
+    speed: speed
+  };
+  var merged = Object.assign(fallback, effectDefaults, configured);
+  var mySwiper = new Swiper (`.swiper-${moduleID}`, merged);
+}
